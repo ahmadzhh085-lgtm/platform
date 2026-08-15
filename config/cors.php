427356ababf3122f7ef3a3a06cac3226f5cca9d2
MouhamadDeop7 <https://github.com/MouhamadDeop7>
2026-08-15 20:07:58 +0300

    Initi87mit

--- config/cors.php
+++ config/cors.php
@@ -8,6 +8,8 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
+        'http://localhost:5174',
+        'http://127.0.0.1:5174',
         'https://zoological-flow-production-40af.up.railway.app',
     ],
 
