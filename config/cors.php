f66fe89fd71c14de38dfdf60efada53d511a9775
MouhamadDeop7 <https://github.com/MouhamadDeop7>
2026-08-16 00:26:28 +0300

    In789it

--- config/cors.php
+++ config/cors.php
@@ -10,6 +10,8 @@ return [
         'http://127.0.0.1:5173',
         'http://localhost:5174',
         'http://127.0.0.1:5174',
+        'http://localhost:5175',
+        'http://127.0.0.1:5175',
         'https://zoological-flow-production-40af.up.railway.app',
     ],
 
